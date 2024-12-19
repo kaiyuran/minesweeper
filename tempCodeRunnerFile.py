e27125cdd4f3e7b861c7b7005d85fd46f9caf156
kaiyuran <73236245+kaiyuran@users.noreply.github.com>
2024-12-19 17:54:33 -0500

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-
-    # # print(clickGrid)
-    # # print(bombGrid)
-    # for yLoc, rowOfCells in enumerate(bombGrid):
-    #     for xLoc, cell in enumerate(rowOfCells):
-    #         if not cell and not clickGrid[yLoc][xLoc]:
-    #             win = False
